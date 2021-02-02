57f3fb5380183adb4b711bea94e3a922e98e6d8b
Eric <ejwinebrenner@gmail.com>
2021-02-02 11:32:17 -0500

    Wk3 Gold Challenges - EFA

---
+++ 3.1-inputDom.instructional/main.js
@@ -0,0 +1,12 @@
+/* 
+    - Using the html and js files provided, write a webpage that asks the user for input on a store, a quantity, and a product.  
+    
+    - Save the user input in 3 separate variables. 
+        - Once the user has input their information, your webpage should console.log a sentence using those 3 variables. Using dom manipulation, you should also display the user input to the screen. For example: 'I shopped at *Target* for *5* *apples*' may be output to the console & screen. 
+        
+    - Do not alter the HTML doc.
+
+    - This does not need to be styled in any sort of fashion, but feel free to if you would like!
+*/
+
+//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
---
+++ 3.2-guessingGame.instructional.js
@@ -0,0 +1,21 @@
+// What is needed for a guesing game?
+
+// 1.  A range for our user to guess between. This should be 1-10.
+// 2.  A randomized computer answer.
+// 3.  A user guess that will provide a prompt
+// 4.  A guess count (for loop variable declaration)
+// 5.  gameLost = false
+
+
+// Logic
+// 1.  A way to validate the user guess
+// 2.  Check if the user guess === the computer answer and alert that he / she won
+// 3.  If the user guess is greater than the computer answer - nudge him or her to guess lower
+// 4.  If the user guess is lower than the computer answer - nudge him or her to guess higher
+// 5.  Handle the case when a user loses
+
+// *** You will need to explore documentation on parseInt(), prompt, alert, Math.floor(), and Math.random()
+
+//? not required:  If you liked to style it, feel free to add a stylesheet to your HTML.
+
+//NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
